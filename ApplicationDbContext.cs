ac608f0921a568227ed94198df96be4b0f683b1d
Towhidul Islam <towhidul.islam@sitback.com.au>
2023-08-22 11:31:39 +1000

    Add project files.

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EventGenerationWithEF
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<RecurringEvent> RecurringEvents { get; set; }
+
+        // Other DbSet properties
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configure the RecurringEvent entity and other configurations
+        }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+        public string DbPath { get; }
+
+        public ApplicationDbContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "blogging.db");
+        }
+
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using EventGenerationWithEF;
+
+public class RecurringEvent
+{
+    public int Id { get; set; }
+    public required string Title { get; set; }
+    public DaysOfWeek DaysOfWeek { get; set; }
+    // Other properties for recurrence pattern
+}
+
+[Flags]
+public enum DaysOfWeek
+{
+    None = 0,
+    Sunday = 1,
+    Monday = 2,
+    Tuesday = 4,
+    Wednesday = 8,
+    Thursday = 16,
+    Friday = 32,
+    Saturday = 64
+}
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        using (var context = new ApplicationDbContext())
+        {
+            int eventId = 1; // ID of the event you're editing
+            var editedEvent = context.RecurringEvents.FirstOrDefault(e => e.Id == eventId);
+
+            if (editedEvent != null)
+            {
+                editedEvent.DaysOfWeek = DaysOfWeek.Monday; // Set to the desired day
+                // Update other recurrence pattern properties if needed
+
+                context.SaveChanges();
+                Console.WriteLine("Event updated successfully.");
+            }
+            else
+            {
+                var recurringEvent = new RecurringEvent { DaysOfWeek = DaysOfWeek.Monday | DaysOfWeek.Tuesday | DaysOfWeek.Friday,Id=2,Title="Title2" };
+                context.RecurringEvents.Add(recurringEvent);
+                context.SaveChanges();
+                Console.WriteLine("Event not found.");
+            }
+        }
+    }
+}
